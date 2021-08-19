f7ebb6e00f1bc050977ef5a7020aad70718a23d0
Usyk <milausik@ukr.net>
2021-08-19 15:19:04 +0300

    added files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './sass/main.scss';
+
+import { alert, info, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+ 
+  success({
+    text: 'Notice me, senpai!'
+  });
