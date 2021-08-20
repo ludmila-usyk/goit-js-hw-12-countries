b795f266ec88f95e2583e464808ae401cb878f10
Usyk <milausik@ukr.net>
2021-08-20 17:45:21 +0300

    added lodash(test))

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import './sass/main.scss';
 
-import { alert, info, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
-import '@pnotify/core/dist/BrightTheme.css';
+// import { alert, info, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
+// import '@pnotify/core/dist/BrightTheme.css';
  
-  success({
-    text: 'Notice me, senpai!'
-  });
+//   success({
+//     text: 'Notice me, senpai!'
+//   });
+
+
+  import debounce from 'lodash.debounce';
+  debounce(test, 2000);
+  
+  function test () {
+  console.log('object')
+};
