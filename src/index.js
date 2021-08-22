26fcfe5a291a44de139c4185ad94f3914bcfefe5
Usyk <milausik@ukr.net>
2021-08-22 12:27:36 +0300

    createCountry

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './sass/main.scss';
-import axios from 'axios';
+import debounce from 'lodash.debounce';
 
 // import { alert, info, success, error } from '../node_modules/@pnotify/core/dist/PNotify.js';
 // import '@pnotify/core/dist/BrightTheme.css';
@@ -8,103 +8,50 @@ import axios from 'axios';
 //     text: 'Notice me, senpai!'
 //   });
 
-
-//   import debounce from 'lodash.debounce';
-//   debounce(test, 2000);
-
+//debounce(test, 2000);
 //   function test () {
 //   console.log('object')
 // };
 
-
-
-
-
-
-
-// Search cocktail by name
-// www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
-
-// List all cocktails by first letter
-// www.thecocktaildb.com/api/json/v1/1/search.php?f=a
-
-// Search ingredient by name
-// www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka
-
-// Lookup full cocktail details by id
-// www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007
-
-// Lookup ingredient by ID
-// www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=552
-
-// Lookup a random cocktail
-// www.thecocktaildb.com/api/json/v1/1/random.php
-
-// Search by ingredient
-// www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
-// www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka
-
-// Filter by alcoholic
-// www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic
-// www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic
-
-// Filter by Category
-// www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink
-// www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail
-
-// Filter by Glass
-// www.thecocktaildb.com/api/json/v1/1/filter.php?g=Cocktail_glass
-// www.thecocktaildb.com/api/json/v1/1/filter.php?g=Champagne_flute
-
 const refs = {
   form: document.querySelector('#form'),
-  input: document.querySelector('#search'),
-  container: document.querySelector('.container'),
-  // more: document.querySelector('#more')
+  input: document.querySelector('.input-js'),
+  countries: document.querySelector('.countries-js'),
 }
 
 // //посылает запрос и обрабатывает ответ
-// const hendlerSubmit = (e) => {
-//   e.preventDefault()
-//   const value = refs.input.value
-// //  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
-//   .then(response => response.json())
-//   .then(result => renderCollection(result.drinks))
-//   .catch(err => console.log(err))
-// }
-
-const hendlerSubmit = (e) => {
+const searchCountry = (e) => {
   e.preventDefault()
-  // innerHTML - очистить разметку
-  // refs.container.innerHTML = '';
-  const value = refs.input.value
-  axios.get(`https:www.thecocktaildb.com/api/json/v1/1/search.php?s=${value}`)
-  .then(result => renderCollection(result.data.drinks))
+  const name = refs.input.value
+  fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+  .then(response => response.json())
+  .then(data => console.log(data))
   .catch(err => console.log(err))
 }
 
+refs.form.addEventListener('submit', searchCountry)
 
-//создает елемент интерфейса
-function createItem ({strDrinkThumb, strDrink, strAlcoholic, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5}) {
+
+// //создает елемент интерфейса
+function createCountry ({flag, name, capital, population, numericCode, region}) {
   const article = `<article>
-    <img src='${strDrinkThumb}' alt='${strDrink}'/>
-    <p>${strDrink}</p>
-    <h3 class="alco">${strAlcoholic}</h3>
+    <img src='${flag}' alt='${name}'/>
+    <h3>${name}</h3>
     <ul>
-      <li>${strIngredient1}</li>
-      <li>${strIngredient2}</li>
-      <li>${strIngredient3}</li>
-      <li>${strIngredient4}</li>
-      <li>${strIngredient5}</li>
+      <li>${capital}</li>
+      <li>${population}</li>
+      <li>${numericCode}</li>
+      <li>${region}</li>
     </ul>
   </article>
 `
-refs.container.insertAdjacentHTML('beforeend', article)
+refs.countries.insertAdjacentHTML('beforeend', article)
 }
 
-//рендерит на экран
-function renderCollection (arr) {
-  arr.forEach(el => createItem(el))
+
+// //рендерит на экран
+function renderCountry (arr) {
+  arr.forEach(el => createCountry(el))
 }
 
-refs.form.addEventListener('submit', hendlerSubmit);
+refs.countries.addEventListener('submit', renderCountry);
